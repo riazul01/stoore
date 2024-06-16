8e531d99c24e49e474305e27b0ab262d791a5ad3
Riazul Islam <riazul5207@gmail.com>
2024-06-16 15:39:04 +0600

    topbar section updated

--- src/layouts/main-layout/Topbar.tsx
+++ src/layouts/main-layout/Topbar.tsx
@@ -10,26 +10,26 @@ const Topbar = () => {
       <p className="text-base font-medium">
         Get <span className="font-bold text-[orange]">30% off</span> for first
         order with exciting gift box!{" "}
-        <Link to="#" className="underline">
+        <Link to="#!" className="underline">
           Learn more
         </Link>
       </p>
 
       {/* topbar links */}
       <div className="flex items-center gap-3">
-        <Link to="#" className="font-medium">
+        <Link to="#!" className="font-medium">
           Login
         </Link>
         <span>|</span>
-        <Link to="#" className="font-medium">
+        <Link to="#!" className="font-medium">
           Register
         </Link>
         <span>|</span>
-        <Link to="#" className="font-medium">
+        <Link to="#!" className="font-medium">
           About Us
         </Link>
         <span>|</span>
-        <Link to="#" className="font-medium">
+        <Link to="#!" className="font-medium">
           Help
         </Link>
       </div>
